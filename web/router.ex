0e5d1acaaef7a2ca485ae94ca209d7c318999620
Shodai Yokoyama <quantumcars@gmail.com>
2015-09-24 10:27:15 +0900

    router.ex: Stop routing to pages

--- web/router.ex
+++ web/router.ex
@@ -15,8 +15,6 @@ defmodule LeanBook.Router do
 
   scope "/", LeanBook do
     pipe_through :browser # Use the default browser stack
-
-    get "/", PageController, :index
   end
 
   # Other scopes may use custom stacks.
