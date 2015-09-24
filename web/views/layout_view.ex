5f5add5d631e3732c55a023e4e0360e3d64b36bc
Shodai Yokoyama <quantumcars@gmail.com>
2015-09-25 00:15:35 +0900

    Use page-specific page tiles

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,7 @@
 defmodule LeanBook.LayoutView do
   use LeanBook.Web, :view
+
+  def page_title(conn, assigns) do
+    render_existing view_module(conn), "page_title." <> view_template(conn), assigns
+  end
 end
