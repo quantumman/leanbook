6592f18b0a7efd5e10650d3c74cfb7963792bb39
Shodai Yokoyama <quantumcars@gmail.com>
2015-10-01 08:31:48 +0900

    RubyServer: Add AsciiDoctor

---
+++ lib/rubyServer/asciiDoctor.ex
@@ -0,0 +1,32 @@
+defmodule RubyServer.AsciiDoctor do
+  def convert(input) do
+    RubyServer.call(
+      ~s"""
+      require('asciidoctor')
+      html = Asciidoctor.convert source, header_footer: true,  safe: 'safe'
+      html || ""
+      """,
+      input
+    )
+    |> Floki.find("body")
+    |> to_html
+  end
+
+  defp to_html(node) do
+    case node do
+      []                           -> ""
+      text when is_bitstring(text) -> text
+      [h|t]                        -> to_html(h) <> to_html(t)
+      {"body", attrs, children}    -> to_html({"main", attrs, children})
+      {elem, attrs, children}      ->
+        "<#{elem}#{to_attr attrs}>#{to_html children}</#{elem}>"
+    end
+  end
+  defp to_attr(attrs) do
+    List.foldl(
+      attrs,
+      "",
+      fn ({key, value}, acc) -> acc <> " #{key}='#{value}'" end
+    )
+  end
+end
