1365d7e515655874dec0805a7b9fc6f07afe449a
Shodai Yokoyama <quantumcars@gmail.com>
2015-10-05 08:04:50 +0900

    article_view: Render a script for show.html

--- web/views/article_view.ex
+++ web/views/article_view.ex
@@ -2,4 +2,8 @@ defmodule LeanBook.ArticleView do
   use LeanBook.Web, :view
 
   def render("page_title.show.html", assigns), do: assigns[:article].title
+
+  def render("scripts.show.html", %{:conn => conn} = assigns), do: ~s"""
+  <script src="#{static_path(conn, "/js/reader.js")}"></script>
+  """
 end
