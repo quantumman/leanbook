defceee09043098e26f385e7e784cf9f1bda0303
Shodai Yokoyama <quantumcars@gmail.com>
2015-10-05 08:01:26 +0900

    Support page-specific scripts

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -4,4 +4,17 @@ defmodule LeanBook.LayoutView do
   def page_title(conn, assigns) do
     render_existing view_module(conn), "page_title." <> view_template(conn), assigns
   end
+
+  def scripts(conn, assigns) do
+    raw(
+        render_existing(view_module(conn), "scripts." <> view_template(conn), assigns)
+        || default_scripts(conn)
+    )
+  end
+
+  def default_scripts(conn) do
+    ~s"""
+    <script src="#{static_path(conn, "/js/app.js")}"></script>
+    """
+  end
 end
