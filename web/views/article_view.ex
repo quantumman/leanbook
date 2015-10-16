e8945637b838b929d4b45b3b6da5b8f8bba32f53
Shodai Yokoyama <quantumcars@gmail.com>
2015-10-16 10:06:35 +0900

    article_view: Render editor.js

--- web/views/article_view.ex
+++ web/views/article_view.ex
@@ -6,4 +6,7 @@ defmodule LeanBook.ArticleView do
   def render("scripts.show.html", %{:conn => conn} = assigns), do: ~s"""
   <script src="#{static_path(conn, "/js/reader.js")}"></script>
   """
+  def render("scripts.new.html", %{:conn => conn} = assigns), do: ~s"""
+  <script src="#{static_path(conn, "/js/editor.js")}"></script>
+  """
 end
