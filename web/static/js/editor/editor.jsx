5dbbba824992055e0bbba6a8f7543f694f574e6f
Shodai Yokoyama <quantumcars@gmail.com>
2015-10-19 07:48:45 +0900

    editor.jsx: Add a model

---
+++ web/static/js/editor/editor.jsx
@@ -0,0 +1,7 @@
+// Model
+let Page = function(data) {
+  this.id = m.prop(data.id);
+  this.chapter = m.prop(data.chapter);
+  this.section = m.prop(data.section);
+  this.markup = m.prop(data.markup);
+};
